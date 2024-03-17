e3c6768e26f427366265452cca3fcb7e36e04788
HasanSohaib-4434 <sohaibhasanpk@gmail.com>
2024-03-17 13:25:36 +0500

    1st commit

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+
+function showSkills() {
+    document.getElementById('content').innerHTML = "<h3>Skills</h3><p>HTML, CSS, JavaScript, Python, Django, C++, C#</p>";
+}
+
+function showExperience() {
+    document.getElementById('content').innerHTML = "<h3>Experience</h3><p>No Experience </p>";
+}
+
+function showEducation() {
+    document.getElementById('content').innerHTML = "<h3>Education</h3><p>Currently doing CS from NUCES</p>";
+}
