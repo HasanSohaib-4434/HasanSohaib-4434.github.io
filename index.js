0dad84a000e3b0355536c8fea77b5ce7ab26d359
HasanSohaib-4434 <sohaibhasanpk@gmail.com>
2024-04-22 10:53:02 +0500

    updated little bit

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 
-function showSkills() {
-    document.getElementById('content').innerHTML = "<h3>Skills</h3><p>HTML, CSS, JavaScript, Python, Django, C++, C#</p>";
-}
-
-function showExperience() {
-    document.getElementById('content').innerHTML = "<h3>Experience</h3><p>No Experience </p>";
-}
-
-function showEducation() {
-    document.getElementById('content').innerHTML = "<h3>Education</h3><p>Currently doing CS from NUCES</p>";
-}
+var typed = new Typed('#element', {
+//   strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  strings: ['Full Stack Developer', 'Android Developer'],
+  typeSpeed: 100,
+  loop:true,
+});
